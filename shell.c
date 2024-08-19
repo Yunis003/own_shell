6d4bcb53158bff33ecaadfea13983ba860b26794
YUnis003 <hi.yunisqulamov@gmail.com>
2024-08-19 08:45:47 +0000

    fixed_bugs

--- shell.c
+++ shell.c
@@ -7,64 +7,66 @@
   *
   * Return: O Always success
   */
-
-int main(int argc, char *argv[])
+int main()
 {
-	(void)argc, (void)argv;
-        char *buf = NULL, *token;
-        size_t count = 0;
-        ssize_t nread;
-	pid_t my_pid;
-	int status, i;
-	char **array;
+    char *buf = NULL;
+    char *token;
+    size_t count = 0;
+    ssize_t nread;
+    pid_t my_pid;
+    int status, i;
+    char **array;
 
-	while (1)
-	{
-        	write(STDOUT_FILENO, "MyShell$ ", 9);
+    while (1)
+    {
+        write(STDOUT_FILENO, "MyShell$ ", 9);
 
-        	nread = getline(&buf, &count, stdin);
+        nread = getline(&buf, &count, stdin);
 
-        	if (nread ==  -1)
-        	{
-                	 perror("Exiting shell");
-                 	 exit(1);
-        	}
+        if (nread == -1)
+        {
+            perror("Exiting shell");
+            exit(1);
+        }
 
-		token = strtok(buf, " \n");
+        token = strtok(buf, " \n");
 
-		array = malloc(sizeof(char) * 1000);
-		i = 0;
+        array = malloc(sizeof(char *) * 1000);
+        i = 0;
 
-		while (token)
-		{
-			array[i]  = token;
-			token = strtok(NULL, " \n");
-			i++;
-		}
+        while (token)
+        {
+            array[i] = token;
+            token = strtok(NULL, " \n");
+            i++;
+        }
 
-		array[i] = NULL;
+        array[i] = NULL;
 
-		my_pid = fork();
+        my_pid = fork();
 
-		if (my_pid == -1)
-		{
-			perror("Failed to create!");
-			exit(41);
-		}
-		if (my_pid == 0)
-		{
-			printf("The creation was succesuful\n");
-			if (execve(array[0], array, NULL) == -1)
-                        {
-                                perror("Failed to execute");
-                                exit(97);
-                        }
-		}
-		else
-		{
-			wait(&status);
-		}
-	}
-        free(buf);
-        return (0);
+        if (my_pid == -1)
+        {
+            perror("Failed to create!");
+            exit(41);
+        }
+        if (my_pid == 0)
+        {
+            printf("The creation was successful\n");
+            if (execve(array[0], array, NULL) == -1)
+            {
+                perror("Failed to execute");
+                exit(97);
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+    }
+
+    free(buf);
+    free(array);
+    return 0;
 }
+
