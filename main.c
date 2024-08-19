42c8930dc13328e4c65e1643f51cc6180a93bbc5
Yunis003 <hi.yunisqulamov@gmail.com>
2024-08-19 16:35:23 +0000

    another_trial

--- main.c
+++ main.c
@@ -1,96 +1,137 @@
 #include "main.h"
 
 /**
- * handle_builtin_commands - Handles built-in commands like echo
- * @array: Command arguments
- *
- * Return: 1 if a built-in command is executed, 0 otherwise
+ * execute_command - func for executing command
+ * @args: arguments to path
+ * @path: path
  */
-
-int handle_builtin_commands(char **array)
+void execute_command(char **args, char *path)
 {
-    if (strcmp(array[0], "echo") == 0)
-    {
-        int i = 1;
-        while (array[i] != NULL)
-        {
-            printf("%s", array[i]);
-            if (array[i + 1] != NULL)
-                printf(" ");
-            i++;
-        }
-        printf("\n");
-        return (1);
-    }
-    return (0);
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			free(path);
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			free(path);
+			perror("wait failed");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
 
-int main()
+/**
+ * parse_command - func for parsing command
+ * @u_command: command to be parsed
+ * @args: arguments to command
+ */
+void parse_command(char *u_command, char **args)
 {
-    char *buf = NULL, *token, *path;
-    size_t count = 0;
-    ssize_t nread;
-    pid_t child_pid;
-    int i, status;
-    char **array;
+	char *command = strtok(u_command, " \t");
+	int i = 0;
 
-    while (1)
-    {
-        printf("$ ");
-        nread = getline(&buf, &count, stdin);
+	args[0] = NULL;
+	while (command != NULL && i < MAX_LEN - 1)
+	{
+		args[i] = command;
+		i++;
+		command = strtok(NULL, " \t");
+	}
+	args[i] = NULL;
+}
 
-        if (nread ==  -1)
-        {
-            exit(0);
-        }
+/**
+ * process_commands - commands processor func
+ * @commands: commands
+ * @commands_array: array for all commands
+ */
+void process_commands(char *commands, char **commands_array)
+{
+	char *command;
+	int a = 0;
 
-        token = strtok(buf, " \n");
+	command = strtok(commands, "\n");
+	while (command != NULL)
+	{
+		commands_array[a] = command;
+		command = strtok(NULL, "\n");
+		a++;
+	}
+	commands_array[a] = NULL;
+}
 
-        array = malloc(sizeof(char*) * 1024);
-        i = 0;
+/**
+ * handle_commands_array - func for handling array of commands
+ * @commands_array: array of commands
+ */
+void handle_commands_array(char **commands_array)
+{
+	int a = 0;
+	char *command;
 
-        while (token)
-        {
-            array[i] = token;
-            token = strtok(NULL, " \n");
-            i++;
-        }
+	if (strcmp(commands_array[a], "exit") == 0)
+		exit(0);
+	else if (strcmp(commands_array[a], "env") == 0)
+		print_env();
+	else
+		while (commands_array[a] != NULL)
+		{
+			command = commands_array[a];
+			if (strcmp(command, "exit") == 0 && a > 0)
+				exit(2);
+			handle_command(command);
+			a++;
+		}
+}
 
-        array[i] = NULL;
+/**
+ * handle_path - func for handling path
+ * @args: arguments to path
+ * @path: path
+ * @path_env: environment path
+ * @found: int variable
+ */
+void handle_path(char **args, char **path, char **path_env, int *found)
+{
+	char *path_token = NULL;
 
-        if (handle_builtin_commands(array))
-        {
-            free(array);
-            continue;
-        }
+	if (*path_env == NULL)
+	{
+		fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+		free(*path_env);
+		free(*path);
+		exit(127);
+	}
+	path_token = strtok(*path_env, ":");
 
-        path = get_file_path(array[0]);
-
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Failed to create.");
-            exit(41);
-        }
-
-        if (child_pid == 0)
-        {
-            if (execve(path, array, NULL) == -1)
-            {
-                perror("Failed to execute");
-                exit(97);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
-
-        free(path);
-        free(array);
-    }
-    free(buf);
-    return (0);
+	while (path_token != NULL)
+	{
+		strcpy(*path, path_token);
+		strcat(*path, "/");
+		strcat(*path, args[0]);
+		if (access(*path, X_OK) != -1)
+		{
+			*found = 1;
+			break;
+		}
+		path_token = strtok(NULL, ":");
+	}
+	free(*path_env);
 }
 
