9cbbefd81b46013a027fc0aedd59312ddf36e25b
YUnis003 <hi.yunisqulamov@gmail.com>
2024-08-19 10:57:29 +0000

    fixing

--- shell.c
+++ shell.c
@@ -7,120 +7,60 @@
   *
   * Return: 0 on success
   */
+
 int main()
 {
-    char *buf = NULL;
-    char *token;
-    size_t count = 0;
-    ssize_t nread;
-    pid_t child_pid;
-    int i, status;
-    char **array;
-    int pipefd[2];
-    int has_pipe = 0;
-    char *cmd2[1024];
-    int k = 0;
+        char *buf = NULL, *token;
+        size_t count = 0;
+        ssize_t nread;
+        pid_t child_pid;
+        int i, status;
+        char **array;
 
-    while (1)
-    {
-        nread = getline(&buf, &count, stdin);
-
-        if (nread == -1)
+        while (1)
         {
-            perror("Exiting shell");
-            exit(1);
-        }
+                nread = getline(&buf, &count, stdin);
 
-        buf[strlen(buf) - 1] = '\0';
-
-        for (i = 0; buf[i]; i++)
-        {
-            if (buf[i] == '|')
-            {
-                has_pipe = 1;
-                buf[i] = '\0';
-                break;
-            }
-        }
-
-        token = strtok(buf, " ");
-        array = malloc(sizeof(char*) * 1024);
-        i = 0;
-
-        while (token)
-        {
-            array[i] = token;
-            token = strtok(NULL, " ");
-            i++;
-        }
-        array[i] = NULL;
-
-        if (has_pipe)
-        {
-            token = strtok(buf + i + 1, " ");
-            while (token)
-            {
-                cmd2[k] = token;
-                token = strtok(NULL, " ");
-                k++;
-            }
-            cmd2[k] = NULL;
-        }
-
-        child_pid = fork();
-
-        if (child_pid == -1)
-        {
-            perror("Failed to create.");
-            exit(41);
-        }
-
-        if (child_pid == 0)
-        {
-            if (has_pipe)
-            {
-                if (pipe(pipefd) == -1)
+                if (nread ==  -1)
                 {
-                    perror("Pipe failed");
-                    exit(1);
+                        perror("Exiting shell");
+                        exit(1);
                 }
 
-                if (fork() == 0)
-                {
-                    dup2(pipefd[1], STDOUT_FILENO);
-                    close(pipefd[0]);
-                    close(pipefd[1]);
-                    execvp(array[0], array);
-                    perror("Exec failed");
-                    exit(1);
-                }
-                else
-                {
-                    wait(NULL);
-                    dup2(pipefd[0], STDIN_FILENO);
-                    close(pipefd[1]);
-                    close(pipefd[0]);
-                    execvp(cmd2[0], cmd2);
-                    perror("Exec failed");
-                    exit(1);
-                }
-            }
-            else
-            {
-                execvp(array[0], array);
-                perror("Exec failed");
-                exit(1);
-            }
-        }
-        else
-        {
-            wait(&status);
-        }
+                token = strtok(buf, " \n");
 
-        free(array);
-        has_pipe = 0;
-    }
+                array = malloc(sizeof(char*) * 1024);
+                i = 0;
 
-    free(buf);
-    return 0;
+                while (token)
+                {
+                        array[i] = token;
+                        token = strtok(NULL, " \n");
+                        i++;
+                }
+
+                array[i] = NULL;
+
+                child_pid = fork();
+
+                if (child_pid == -1)
+                {
+                        perror("Failed to create.");
+                        exit (41);
+                }
+		if (child_pid == 0)
+		{
+			if (execve(array[0], array, NULL) == -1)
+			{
+				perror("No such file or directory");
+				exit(97);
+			}
+		}
+		else
+		{
+			wait(&status);
+		}
+        }
+        free(buf);
+        return (0);
 }
